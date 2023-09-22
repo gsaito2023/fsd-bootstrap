d9bbdfedae6357a5213ee18e529417ae06d5c21c
George Saito <gsaito12600@gmail.com>
2023-09-21 17:54:03 -0700

    Removed data file from Exericse 22

--- exercise-22/digiprofilecra/src/data.js
+++
@@ -1,6 +0,0 @@
-export const pageLinks = [
-  { id: 1, href: "#professional-experience", text: "Professional Experience" },
-  { id: 2, href: "#technology-summary", text: "Technology Summary" },
-  { id: 3, href: "#certifications", text: "Certifications" },
-  { id: 4, href: "./contact-me.html", text: "Contact Me" },
-];
